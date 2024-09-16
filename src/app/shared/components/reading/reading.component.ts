937e365badd4fa1b0539cd447286fb9389f08bbf
Andriy <andriypanasyk02@gmail.com>
2024-09-16 18:09:43 +0300

    feat: reading highlight text (in progress)

--- src/app/shared/components/reading/reading.component.ts
+++ src/app/shared/components/reading/reading.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { readingData } from '../../data';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
@@ -16,7 +16,11 @@ export class ReadingComponent {
   reading?: ReadingDTO;
   toggleText: boolean = true;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) {
+    document.onmouseup = () => {
+      this.onSelectEventHandler();
+    };
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe((params) => {
@@ -29,5 +33,54 @@ export class ReadingComponent {
 
   toggleModuleText() {
     this.toggleText = !this.toggleText;
+  }
+
+  isSelected(range: Range): boolean {
+    const span = document.createElement('span');
+    span.appendChild(range.cloneContents());
+    document.body.appendChild(span);
+    const isSelected =
+      window.getComputedStyle(span).backgroundColor === 'rgb(255, 255, 0)';
+    document.body.removeChild(span);
+    return isSelected;
+  }
+
+  setColorToRange(color: string): void {
+    const selection = window.getSelection();
+    if (selection && selection.rangeCount > 0) {
+      for (let i = 0; i < selection.rangeCount; i++) {
+        const range = selection.getRangeAt(i);
+        const span = document.createElement('span');
+        span.style.backgroundColor = color;
+        range.surroundContents(span);
+      }
+    }
+  }
+
+  onSelectEventHandler(): void {
+    const selection = window.getSelection();
+    if (selection && selection.rangeCount > 0) {
+      const range = selection.getRangeAt(0);
+      const parentElement = range.commonAncestorContainer.parentElement;
+      if (
+        parentElement &&
+        parentElement.classList.contains('reading__passage--text')
+      ) {
+        if (this.isSelected(range)) {
+          this.setColorToRange('unset');
+        } else {
+          this.setColorToRange('yellow');
+        }
+      }
+      selection.removeAllRanges();
+    }
+  }
+
+  // not reseting the color of the selected text
+  @HostListener('document:keydown', ['$event'])
+  handleKeydown(event: KeyboardEvent): void {
+    if (event.key === 'R' || event.key === 'r') {
+      this.setColorToRange('unset');
+    }
   }
 }

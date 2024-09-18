29e9c3aa73d706ff86e31774f89731f7c827881c
Andriy <andriypanasyk02@gmail.com>
2024-09-18 18:52:06 +0300

    feat: env

--- src/app/shared/components/feedback/feedback.service.ts
+++ src/app/shared/components/feedback/feedback.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import emailjs from 'emailjs-com';
+import { environment } from '../../../../envorinments/envorinments';
+
 @Injectable({
   providedIn: 'root',
 })
 export class FeedbackService {
-  private serviceId = 'service_cbzoe5b'; // Ensure this is the correct service ID
-  private templateId = 'template_5ahx7eq'; // Ensure this is the correct template ID
-  private publicKey = '7D878_ni0EDR94-6l'; // Ensure this is the correct public key
+  private serviceId = environment.serviceId;
+  private templateId = environment.templateId;
+  private publicKey = environment.publicKey;
 
   constructor() {}
 
--- src/envorinments/envorinments.ts
+++ src/envorinments/envorinments.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:4200',
+  serviceId: 'service_cbzoe5b',
+  templateId: 'template_5ahx7eq',
+  publicKey: '7D878_ni0EDR94-6l',
 };
